efbd7cd2746ef9746e7ee0ed278806e659d5b7d1
LynnFMS <lynn1312@gmail.com>
2024-12-04 19:12:42 -0500

    fix

--- frontEnd/tailwind.config.js
+++ frontEnd/tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-<<<<<<< Updated upstream
   content: [
     "./src/**/*.{js,jsx}"
   ],
-=======
-  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
->>>>>>> Stashed changes
   theme: {
     extend: {},
   },
