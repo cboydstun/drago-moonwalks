743e8ddfe4ef8caaa8f5d05c93d414c6d3775b5c
Elliott Nottingham <elliottnottingham07@gmail.com>
2024-12-11 17:24:53 -0600

    tailwind

--- frontEnd/src/routes/root.jsx
+++ frontEnd/src/routes/root.jsx
@@ -22,7 +22,7 @@ export default function Root() {
                 <button className="bg-red-600 hover:bg-white hover:text-red-600 text-white font-bold py-1 px-4 rounded-full">Admin</button>
                 </NavLink>
             <img className="rounded-sm absolute top-6 left-6 w-60 h-auto sm:w-40 md:w-40 lg:w-52 xl:w-60"
-                src="https://media.discordapp.net/attachments/1303146516294270996/1309332345635213394/image.png?ex=67413245&is=673fe0c5&hm=d27f00db3f87f1b7e08a3dedc6adc2daf61d9a7f8df0d4ffff33135f82e01f42&=&format=webp&quality=lossless&width=372&height=352"
+                src=""
                 alt="Dragon Picture"/>
             </main>
             <CloudinaryComponent></CloudinaryComponent>
