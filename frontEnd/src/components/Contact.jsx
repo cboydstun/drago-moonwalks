5baf3e5f35a6dd2d9f696de9401f4d3fe37d5d1b
Elliott Nottingham <elliottnottingham07@gmail.com>
2024-12-30 15:55:05 -0600

    updated the styling and layout to fit all screen sizes

--- frontEnd/src/components/Contact.jsx
+++ frontEnd/src/components/Contact.jsx
@@ -1,12 +1,18 @@
 export default function Contact() {
     return (
-        <main className="flex flex-col self-center items-center justify-items-center ml-10">
-            <h1 className="text-white font-bold text-4xl font-serif">Contact</h1>
-                <div className="flex flex-col items-center p-6">
-                    <h2 className="text-white font-bold text-2xl font-serif p-4">Phone Number: 210-633-7867</h2>
-                    <h2 className="text-white font-bold text-2xl font-serif p-4">Email: dragos.moonwalks@yahoo.com</h2>
-                    <a className="text-white font-bold text-2xl font-serif mt-4 border bg-blue-600 rounded-xl p-1 hover:text-blue-600 hover:bg-white" target="blank" href="https://facebook.com/DragosMoonwalks">Facebook Page</a>
-                </div>
+        <main className="flex flex-col items-center justify-center p-6 sm:p-12">
+            <h1 className="text-white font-bold text-4xl sm:text-5xl font-serif mb-6">Contact</h1>
+            <div className="flex flex-col items-center p-4">
+                <h2 className="text-white font-bold text-2xl sm:text-3xl font-serif p-4">Phone Number: 210-633-7867</h2>
+                <h2 className="text-white font-bold text-2xl sm:text-3xl font-serif p-4">Email: dragos.moonwalks@yahoo.com</h2>
+                <a 
+                    className="text-white font-bold text-2xl sm:text-3xl font-serif mt-4 border bg-blue-600 rounded-xl p-2 hover:text-blue-600 hover:bg-white"
+                    target="blank"
+                    href="https://facebook.com/DragosMoonwalks"
+                >
+                    Facebook Page
+                </a>
+            </div>
         </main>
-    )
-};
+    );
+}
--- frontEnd/src/routes/root.jsx
+++ frontEnd/src/routes/root.jsx
@@ -1,50 +1,65 @@
 import { NavLink, Outlet } from "react-router-dom";
 
 export default function Root() {
-    return(
-      
-    <div className="grid grid-cols-1 items-center min-h-screen bg-gradient-to-r from-blue-500 to-green-400 text-right bg-auto">
-        <img className="rounded-sm absolute top-6 left-6 w-60 h-auto sm:w-40 md:w-40 lg:w-52 xl:w-60"
-            src="./src/routes/assets/DragoPic.png"
-            alt="Dragon Picture"/>
-        <header className="w-full">
-            <h1 className="text-center text-white font-bold text-6xl font-serif">Drago's Moonwalks</h1>
-            <h3 className="text-center text-white font-bold text-2xl my-4 font-serif">Parties Fundraisers and Family Reunions</h3>
-        </header>
-        <main className="p-6">
-            <div className="flex flex-grow justify-center gap-x-4">
-                <NavLink to="/contact">
-                    <button className="bg-yellow-600 hover:bg-white hover:text-yellow-600 text-white font-bold py-1 px-4 rounded-full">Contact</button>
-                </NavLink>
-                <NavLink to="/booking">
-                    <button className="bg-yellow-600 hover:bg-white hover:text-yellow-600 text-white font-bold py-1 px-4 rounded-full">Bookings & Availability</button>
-                </NavLink>
-                <NavLink to="/login">
-                <button className="bg-red-600 hover:bg-white hover:text-red-600 text-white font-bold py-1 px-4 rounded-full">Admin</button>
-                </NavLink>
+    return (
+        <div className="min-h-screen bg-gradient-to-r from-blue-500 to-green-400 text-white">
+            <div className="flex items-center justify-between px-6 py-6 sm:py-12">
+                <img
+                    className="w-32 h-auto sm:w-40 md:w-52 lg:w-60 xl:w-60"
+                    src="./src/routes/assets/DragoPic.png"
+                    alt="Dragon Picture"
+                />
+                <div className="text-center sm:text-left">
+                    <h1 className="text-4xl sm:text-6xl font-bold font-serif">Drago's Moonwalks</h1>
+                    <h3 className="text-xl sm:text-2xl my-4 font-serif text-center">Parties Fundraisers and Family Reunions</h3>
+                </div>
             </div>
-        </main>
-
-
-
-        <div className="flex">
-            <main className="text-left m-14 max-w-48">
-                <NavLink to="/all">
-                    <button className="bg-yellow-600 text-white rounded-full w-40 transition-transform transform hover:scale-105 hover:bg-white hover:text-yellow-600 font-bold">All</button>
-                </NavLink>
-                <NavLink>
-                    <button className="bg-yellow-600 text-white rounded-full w-40 transition-transform transform hover:scale-105 hover:bg-white hover:text-yellow-600 mt-4 font-bold">Bouncy Houses & Moonwalks</button>
-                </NavLink>
-                <NavLink>
-                    <button className="bg-yellow-600 text-white rounded-full w-40 transition-transform transform hover:scale-105 hover:bg-white hover:text-yellow-600 mt-4 font-bold">Tables & Chairs</button>
-                </NavLink>
-                <NavLink>
-                    <button className="bg-yellow-600 text-white rounded-full w-40 transition-transform transform hover:scale-105 hover:bg-white hover:text-yellow-600 mt-4 font-bold">Appliances</button>
-                </NavLink>
+            <main className="p-6">
+                <div className="flex flex-wrap justify-center gap-4">
+                    <NavLink to="/contact">
+                        <button className="bg-yellow-600 hover:bg-white hover:text-yellow-600 text-white font-bold py-1 px-6 rounded-full">
+                            Contact
+                        </button>
+                    </NavLink>
+                    <NavLink to="/booking">
+                        <button className="bg-yellow-600 hover:bg-white hover:text-yellow-600 text-white font-bold py-1 px-6 rounded-full">
+                            Bookings & Availability
+                        </button>
+                    </NavLink>
+                    <NavLink to="/login">
+                        <button className="bg-red-600 hover:bg-white hover:text-red-600 text-white font-bold py-1 px-6 rounded-full">
+                            Admin
+                        </button>
+                    </NavLink>
+                </div>
             </main>
-            <Outlet></Outlet>
+            <div className="flex flex-col sm:flex-row justify-between mt-10">
+                <aside className=" w-full sm:w-1/4 p-6">
+                    <NavLink to="/all">
+                        <button className="bg-yellow-600 text-white w-full mb-4 py-2 px-4 rounded-full hover:bg-white hover:text-yellow-600 font-bold">
+                            All
+                        </button>
+                    </NavLink>
+                    <NavLink>
+                        <button className="bg-yellow-600 text-white w-full mb-4 py-2 px-4 rounded-full hover:bg-white hover:text-yellow-600 font-bold">
+                            Bouncy Houses & Moonwalks
+                        </button>
+                    </NavLink>
+                    <NavLink>
+                        <button className="bg-yellow-600 text-white w-full mb-4 py-2 px-4 rounded-full hover:bg-white hover:text-yellow-600 font-bold">
+                            Tables & Chairs
+                        </button>
+                    </NavLink>
+                    <NavLink>
+                        <button className="bg-yellow-600 text-white w-full mb-4 py-2 px-4 rounded-full hover:bg-white hover:text-yellow-600 font-bold">
+                            Appliances
+                        </button>
+                    </NavLink>
+                </aside>
+                <main className="flex-1 p-6">
+                    <Outlet />
+                </main>
+            </div>
         </div>
-    </div>
-    )
+    );
 }
-
